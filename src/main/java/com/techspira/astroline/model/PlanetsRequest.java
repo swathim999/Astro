7f979893aa9017cb695e3bc5b24bcd24bf6e5810
swathi <swathim11@gmail.com>
2024-12-10 20:33:41 -0800

    Updated code

--- src/main/java/com/techspira/astroline/model/PlanetsRequest.java
+++ src/main/java/com/techspira/astroline/model/PlanetsRequest.java
@@ -6,10 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@AllArgsConstructor // Generates a constructor with all fields as parameters
+@NoArgsConstructor  // Generates a no-argument constructor
+@ToString           // Generates a toString method
 public class PlanetsRequest {
-
-
+    // Define your fields here
+    private String name;
+    private int position;
 }
--- src/main/java/com/techspira/astroline/rest/PlanetsController.java
+++ src/main/java/com/techspira/astroline/rest/PlanetsController.java
@@ -1,10 +1,14 @@
 package com.techspira.astroline.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlanetsController {
 
-    public void
-
+    // Example method to fix the issue
+    @GetMapping("/planets")
+    public String getPlanets() {
+        return "Here are some planets";
+    }
 }
